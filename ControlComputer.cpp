460457a8f347b97c009692e6ebb5252c8cc4eb63
alexandershuping <alexandershuping@gmail.com>
2016-08-20 16:01:47 -0400

    Test of parameter processing -- Still nonfuncitnoal
    Program now processes parameters correctly.

--- ControlComputer.cpp
+++ ControlComputer.cpp
@@ -6,7 +6,7 @@
 * By Alexander Shuping
 * 
 * First Write: 19 August 2016
-* Last Change: 19 August 2016
+* Last Change: 20 August 2016
 * 
 * Version History:
 *   0.1 -- First version
@@ -20,14 +20,158 @@
 
 #include "ControlComputer.h"
 
-int main(){
-  int number_of_readings = -1;
+bool streq(std::string a, std::string b){
+  int len;
+	if(a.length() == b.length()){
+    len = a.length();
+	}else{
+    return false;
+	}
+
+	for(int i = 0; i < len; i++){
+    if(a.at(i) != b.at(i)){
+      return false;
+		}
+	}
+	return true;
+}
+
+int main(int argc, char* argv[]){
+  std::signal(SIGINT, handleSafeTermination); // Add a handler for signal-2 termination
+
+  const params p = processParams(argc, argv);                   // Process command-line parameters
+	/*const config c = loadConfig(&p);                              // Load a configuration file
+	establishSerial(&p, &c);                                      // Establish serial communication with Arduino and send the config file
   
-  std::signal(SIGINT, handleSafeTermination);
+	if(c.numberOfScans == -1){                                    // If config specifies infinite readings
+    
+		while(true){ doScan(&p, &c); }                              // Scan until signal 2 is caught
+	
+	}else{
+    
+		for(int i = 0; i < c.numberOfScans; i++){ doScan(&p, &c); } // Perform the specified number of scans
+	
+	}*/
+	std::cout << "Config: " << p.configFile;
+	if(!p.toSTDOUT){std::cout << " | Output: " << p.outputFile;}
+	std::cout << " | Output to STDOUT: " << p.toSTDOUT
+	          << " | Serial port: " << p.serialPort << std::endl;
+}
+
+/**
+* params processParams(int argc, char* argv[])
+*
+* Processes command-line arguments
+* 
+* PARAMS:
+*  int argc     -- argument count
+*  char* argv[] -- argument vector (array of arguments)
+* 
+* RETURN:
+*  params       -- structure containing processed parameters
+* 
+* ERRORS:
+*  Prints usage and force-exits if the program is given improper parameters
+*/
+params processParams(int argc, char* argv[]){
+  if(argc < 3){
+    std::cout << "Missing parameters.\n"
+		          << "Usage: ControlComputer [-o <output>] [-s <serial>] -c config\n"
+		          << "Run man ControlComputer for more information\n\n";
+		exit(1);
+	}
+
+	params outParams;
+	  outParams.configFile = "NULL";
+		outParams.outputFile = "NULL";
+		outParams.toSTDOUT   = true;
+		outParams.serialPort = -1  ;
+
+  for(int i = 1 /*Not a mistake, argv[0] is skipped because it is just the command name*/; i < argc; i++){
+    if(streq(argv[i], "-s")){
+      
+			try{
+      
+			  outParams.serialPort = atoi(argv[i+1]);
+				i++;
+			
+			}catch(...){
+       
+			  std::cout << "Paramater -s must be followed by an integer\n"
+			            << "Usage: ControlComputer [-o <output>] [-s <serial>] -c config\n"
+			            << "Run man ControlComputer for more information\n\n";
+        exit(1);
+        
+			}
+		
+		}else if(streq(argv[i], "-c")){
+
+			try{
+      
+			  outParams.configFile = argv[i+1];
+				i++;
+			
+			}catch(...){
+       
+			  std::cout << "Paramater -c must be followed by a file path\n"
+			            << "Usage: ControlComputer [-o <output>] [-s <serial>] -c config\n"
+			            << "Run man ControlComputer for more information\n\n";
+        exit(1);
+        
+			}
+		}else if(streq(argv[i], "-o")){
+
+			try{
+      
+			  outParams.outputFile = argv[i+1];
+				outParams.toSTDOUT   = false;
+				i++;
+			
+			}catch(...){
+       
+			  std::cout << "Paramater -o must be followed by a file path\n"
+			            << "Usage: ControlComputer [-o <output>] [-s <serial>] -c config\n"
+			            << "Run man ControlComputer for more information\n\n";
+        exit(1);
+        
+			}
+		}else{
+      std::cout << argv[i] << " is not a valid parameter for this program.\n"
+			          << "Usage: ControlComputer [-o <output>] [-s <serial>] -c config\n"
+			          << "Trying to continue anyway...\n\n";
+		}
+	}
+
+  if(!streq(outParams.configFile, "NULL") && (!streq(outParams.outputFile, "NULL") || outParams.toSTDOUT)){
+    
+		return outParams;
+	
+	}else{
+
+			  std::cout << "Invalid or missing parameters.\n"
+			            << "Usage: ControlComputer [-o <output>] [-s <serial>] -c config\n"
+			            << "Run man ControlComputer for more information\n";
+        exit(1);
+	
+	}
+}
+
+/**
+* config processConfig(const params&)
+* 
+* Opens and processes the given config file
+* 
+* PARAMS:
+*  const params& -- Const reference to the commandline parameters
+* 
+* RETURN:
+*  const config& -- Const reference to the processed config options
+* 
+* ERRORS:
+*  Prints usage and exits if config file is improperly-formatted
+*/
+config processConfig(const params&){
   
-	while(true){std::cout << "Testing... terminate this program now...\n";}
-  //activateUI();
-  //establishSerial();     //Establish serial communication with Arduino 
 }
 
 /**
--- ControlComputer.h
+++ ControlComputer.h
@@ -1,27 +1,71 @@
 #pragma once
+#include <string>
 
-enum DATA_STATE{
-  DATA_NUL,       //No data in structure yet
-	DATA_RCV,       //Data received, but not processed
-	DATA_PRO,       //Data processed
-	DATA_BAD        //Data corrupt
+
+/**
+* ENUMS
+*/
+
+
+//Type of scan to perform
+enum SCAN_TYPE{
+  SCAN_ZIGZAG,           // 
+	SCAN_HORZ_ONLY_ZIGZAG,
+	SCAN_VERT_ONLY_ZIGZAG
 };
 
+// State of a data packet
+enum DATA_STATE{
+  DATA_NUL,       // No data in structure yet
+	DATA_RCV,       // Data received, but not processed
+	DATA_PRO,       // Data processed
+	DATA_BAD        // Data corrupt
+};
+
+
+/**
+* STRUCTURES
+*/
+
+
+// Data recieved from a single point LiDAR scan
 struct lidarData{
   
-	DATA_STATE state; //State of data -- see enum DATA_STATE above for details
+	DATA_STATE state; // State of data -- see enum DATA_STATE above for details
 		
-	//Unprocessed readings. Do not access unless state is DATA_RCV or DATA_PRO.
-	double vHorz;     //Horizontal-axis micromirror voltage
-	double vVert;     // Vertical -axis micromirror voltage
-	double distance;  //Distance to target point
+	// Unprocessed readings. Do not access unless state is DATA_RCV or DATA_PRO.
+	double vHorz;     // Horizontal-axis micromirror voltage
+	double vVert;     //  Vertical -axis micromirror voltage
+	double distance;  // Distance to target point
 
-	//Processed readings. Do not access unless state is DATA_PRO. 
-  double x;         //x-coordinate of target point
-	double y;         //y-coordinate of target point
-	double z;         //z-coordinate of target point
+	// Processed readings. Do not access unless state is DATA_PRO. 
+  double x;         // x-coordinate of target point
+	double y;         // y-coordinate of target point
+	double z;         // z-coordinate of target point
 
 };
 
-//Function Prototypes
-void handleSafeTermination(int);
+// Command-line parameters
+struct params{
+  std::string configFile; // Location of config file
+  std::string outputFile; // Location of output file
+	bool        toSTDOUT;   // Output to a file or to STDOUT
+	int         serialPort; // Serial port to connect over
+};
+
+struct config{
+  SCAN_TYPE scanType;       // Type of scan to perform
+	int       scanResolution; // Resolution of scan (in delta-volts per scan). In config file, this is specified in delta-angle per scan.
+	int       numberOfScans;  // Number of complete scans to perform, or -1 to scan until signal 2 is caught
+};
+
+
+/**
+* FUNCTION PROTOTYPES
+*/
+
+
+void   handleSafeTermination (int sig)                      ;
+params processParams         (int argc, char* argv[])       ;
+config processConfig         (const params&)                ;
+void   doScan                (const params&, const config&) ;
